944b8363cde94386f93e97804975e6a5e6cf6cb6
topmoneymaker <michaelwinkler.business@gmail.com>
2025-02-17 20:35:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def scrape_facebook_ads(keyword):
     """Scrapes Facebook Ad Library for ads based on the given keyword."""
 
     # Paths for Chrome and Chromedriver
-    CHROME_PATH = "/opt/render/chrome/chrome-linux/chrome"
+    CHROME_PATH = "/opt/render/chrome/opt/google/chrome/google-chrome"
     CHROMEDRIVER_PATH = "/opt/render/chrome/chromedriver"
 
     # Ensure Chromedriver exists
