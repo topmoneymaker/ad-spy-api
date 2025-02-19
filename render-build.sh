9d1df254cd42cc70282ba07da365cef0d08b9000
topmoneymaker <michaelwinkler.business@gmail.com>
2025-02-19 06:13:38 +0100

    Recreated render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Ensure APT is available
+apt-get update -y
+
+# Install Google Chrome
+apt-get install -y google-chrome-stable
+
+# Install Chromedriver (automatically picks the correct version)
+apt-get install -y chromium-chromedriver
+
+# Verify Installation
+which chromedriver
+which google-chrome
